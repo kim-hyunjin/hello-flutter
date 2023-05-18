b96ac2acd178b580f8e4e066db897ea4fc3cbfe9
kim-hyunjin <hyunjin1612@gmail.com>
2023-05-18 17:54:33 +0900

    create widgets

---
+++ roll_dice_app/lib/dice_roller.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+// _ mean only can use in this file's scope
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/images/dice-1.png';
+
+  void rollDice() {
+    setState(() {
+      activeDiceImage = 'assets/images/dice-4.png';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          activeDiceImage,
+          width: 200,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          // style:
+          //     TextButton.styleFrom(padding: const EdgeInsets.only(top: 20)),
+          child: const StyledText('press'),
+        )
+      ],
+    );
+  }
+}
