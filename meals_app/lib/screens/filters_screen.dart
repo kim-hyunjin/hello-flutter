440cf43850dc5f2a4b7a8064081d3a93f75667d8
kim-hyunjin <hyunjin1612@gmail.com>
2023-05-28 21:21:37 +0900

    feat: filter item

--- meals_app/lib/screens/filters_screen.dart
+++ meals_app/lib/screens/filters_screen.dart
@@ -13,6 +13,9 @@ class FiltersScreen extends StatefulWidget {
 
 class _FiltersScreenState extends State<FiltersScreen> {
   var _glutenFreeFilterSet = false;
+  var _lactoseFreeFilterSet = false;
+  var _vegeterianFilterSet = false;
+  var _veganFilterSet = false;
 
   @override
   Widget build(BuildContext context) {
@@ -31,31 +34,81 @@ class _FiltersScreenState extends State<FiltersScreen> {
       //   },
       // ),
       body: Column(children: [
-        SwitchListTile(
-          value: _glutenFreeFilterSet,
-          onChanged: (isChecked) {
-            setState(() {
-              _glutenFreeFilterSet = isChecked;
-            });
-          },
-          title: Text(
-            'Gluten-free',
-            style: Theme.of(context)
-                .textTheme
-                .titleLarge!
-                .copyWith(color: Theme.of(context).colorScheme.onBackground),
-          ),
-          subtitle: Text(
-            'Only include gluten-free meals.',
-            style: Theme.of(context)
-                .textTheme
-                .labelMedium!
-                .copyWith(color: Theme.of(context).colorScheme.onBackground),
-          ),
-          activeColor: Theme.of(context).colorScheme.tertiary,
-          contentPadding: const EdgeInsets.only(left: 34, right: 32),
-        ),
+        FilterItem(
+            label: 'Gluten-free',
+            desc: 'Only include gluten-free meals.',
+            value: _glutenFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _glutenFreeFilterSet = isChecked;
+              });
+            }),
+        FilterItem(
+            label: 'Lactose-free',
+            desc: 'Only include lactose-free meals.',
+            value: _lactoseFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _lactoseFreeFilterSet = isChecked;
+              });
+            }),
+        FilterItem(
+            label: 'Vegeterian',
+            desc: 'Only include vegeterian meals.',
+            value: _vegeterianFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _vegeterianFilterSet = isChecked;
+              });
+            }),
+        FilterItem(
+            label: 'Vegan',
+            desc: 'Only include vegan meals.',
+            value: _veganFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _veganFilterSet = isChecked;
+              });
+            }),
       ]),
+    );
+  }
+}
+
+class FilterItem extends StatelessWidget {
+  const FilterItem(
+      {super.key,
+      required this.label,
+      required this.desc,
+      required this.value,
+      required this.onChanged});
+
+  final String label;
+  final String desc;
+  final bool value;
+  final void Function(bool isChecked) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return SwitchListTile(
+      value: value,
+      onChanged: onChanged,
+      title: Text(
+        label,
+        style: Theme.of(context)
+            .textTheme
+            .titleLarge!
+            .copyWith(color: Theme.of(context).colorScheme.onBackground),
+      ),
+      subtitle: Text(
+        desc,
+        style: Theme.of(context)
+            .textTheme
+            .labelMedium!
+            .copyWith(color: Theme.of(context).colorScheme.onBackground),
+      ),
+      activeColor: Theme.of(context).colorScheme.tertiary,
+      contentPadding: const EdgeInsets.only(left: 34, right: 32),
     );
   }
 }
