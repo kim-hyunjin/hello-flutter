28de5b62655e9d907c1dbfed212590358c1af4b4
kim-hyunjin <hyunjin1612@gmail.com>
2023-05-28 13:23:25 +0900

    불필요한 build 메소드 호출 줄이기

--- todo_app/lib/ui_updates_demo.dart
+++ todo_app/lib/ui_updates_demo.dart
@@ -1,91 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/demo_buttons.dart';
 
-class UIUpdatesDemo extends StatefulWidget {
+class UIUpdatesDemo extends StatelessWidget {
   const UIUpdatesDemo({super.key});
 
   // flutter call this method to create the element for this widget
   // if widget stay in the ui, doesn't call anymore
   @override
-  StatefulElement createElement() {
+  StatelessElement createElement() {
     print('UIUpdatesDemo CREATEELEMENT called');
     return super.createElement();
   }
 
   @override
-  State<UIUpdatesDemo> createState() {
-    return _UIUpdatesDemo();
-  }
-}
-
-class _UIUpdatesDemo extends State<UIUpdatesDemo> {
-  var _isUnderstood = false;
-
-  @override
   Widget build(BuildContext context) {
     print('UIUpdatesDemo BUILD called');
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
       child: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
+            Text(
               'Every Flutter developer should have a basic understanding of Flutter\'s internals!',
               textAlign: TextAlign.center,
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 16),
-            const Text(
+            SizedBox(height: 16),
+            Text(
               'Do you understand how Flutter updates UIs?',
               textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    // build method called after setState call everytime
-                    setState(() {
-                      _isUnderstood = false;
-                    });
-                  },
-                  child: const Text('No'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _isUnderstood = true;
-                    });
-                  },
-                  child: const Text('Yes'),
-                ),
-              ],
-            ),
-            if (_isUnderstood) const AwesomeText(),
+            SizedBox(height: 24),
+            DemoButtons(),
           ],
         ),
       ),
     );
-  }
-}
-
-// 위에서 버튼을 눌러 이 위젯을 사라지게하고 나타나게 함
-class AwesomeText extends StatelessWidget {
-  const AwesomeText({super.key});
-
-  // tree에서 사라졌다가 다시 나타날때마다 호출됨.
-  // build 메소드 이전에 호출됨.
-  @override
-  StatelessElement createElement() {
-    print('AwesomeText createElement called');
-    return super.createElement();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('AwesomeText build called');
-    return const Text('Awesome!');
   }
 }
