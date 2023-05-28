28773ed67c2ffc58610041eee058b676be412fb1
kim-hyunjin <hyunjin1612@gmail.com>
2023-05-28 16:26:34 +0900

    feat: navigate to meals screen

--- meals_app/lib/screens/categories_screen.dart
+++ meals_app/lib/screens/categories_screen.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_category.dart';
+import 'package:meals_app/data/dummy_meals.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals_screen.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final meals = dummyMeals
+        .where((element) => element.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: meals,
+        ),
+      ),
+    ); // Navigator.push(context, route);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +37,12 @@ class CategoriesScreen extends StatelessWidget {
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
         children: availableCategories
-            .map((category) => CategoryGridItem(category: category))
+            .map((category) => CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectCategory(context, category);
+                  },
+                ))
             .toList(),
       ),
     );
