651056721a1053fbf6ef0fea314fb9d6616e112b
kim-hyunjin <hyunjin1612@gmail.com>
2023-05-28 13:37:19 +0900

    add todos widgets

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/todos/todos.dart';
 
 import 'package:todo_app/ui_updates_demo.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter Internals'),
         ),
-        body: const UIUpdatesDemo(),
+        body: const Todos(),
       ),
     );
   }
