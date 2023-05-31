2cfd46c61c91162384b9d030202cc8c9864b38d0
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-01 01:37:55 +0900

    show google map in detail screen

--- favorite_places/lib/place_detail_screen.dart
+++ favorite_places/lib/place_detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/map_screen.dart';
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 
@@ -26,9 +27,21 @@ class PlaceDetailScreen extends StatelessWidget {
             right: 0,
             child: Column(
               children: [
-                CircleAvatar(
-                  radius: 70,
-                  backgroundImage: NetworkImage(place.location.locationImage),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (ctx) => MapScreen(
+                          location: place.location,
+                          isSelecting: false,
+                        ),
+                      ),
+                    );
+                  },
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(place.location.locationImage),
+                  ),
                 ),
                 Container(
                   alignment: Alignment.center,
