c71c54b1c063297ae906d3543d6f4da0c332acc2
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-01 22:38:43 +0900

    feat: save chat in firestore

--- chat_app/lib/screens/chat_screen.dart
+++ chat_app/lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -20,9 +22,10 @@ class ChatScreen extends StatelessWidget {
               ))
         ],
       ),
-      body: const Center(
-        child: Text('Chat'),
-      ),
+      body: Column(children: [
+        Expanded(child: ChatMessages()),
+        NewMessage(),
+      ]),
     );
   }
 }
