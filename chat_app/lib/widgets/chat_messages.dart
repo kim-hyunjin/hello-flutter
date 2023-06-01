afcf53e3f0adb3235902ccb37a6eb0e85736264d
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-01 22:59:22 +0900

    feat: display chat message

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -1,12 +1,41 @@
-import 'package:flutter/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('No messages found.'),
-    );
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chat')
+            .orderBy('createdAt', descending: false)
+            .snapshots(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(
+              child: Text('No messages found.'),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Something went wrong...'),
+            );
+          }
+
+          final loadedMsg = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: loadedMsg.length,
+            itemBuilder: (ctx, index) => Text(loadedMsg[index].data()['text']),
+          );
+        });
   }
 }
